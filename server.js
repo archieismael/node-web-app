032b13de8bf3b8424b8851899f33aa618309d79d
root <archie_ismael@yahoo.com>
2021-05-04 00:24:36 +0000

    Updated Application

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send('Hello World! This portal is run from Docker Container.');
 });
 
 app.listen(PORT, HOST);
